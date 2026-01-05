2c9c5d9b47001aa0ec7b94bb24ae790b80986224
Ben <bensantos139@gmail.com>
2026-01-05 23:24:52 +0800

    Setup and added 1st class

---
+++ src/resources/block.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+/**
+ * Generates a tree of block.
+ */
+class block {
+    public:
+        // Constructor
+        block();
+
+        void generate(){
+
+        };
+
+    private:
+        int m_value;
+        std::string m_name;
+};
